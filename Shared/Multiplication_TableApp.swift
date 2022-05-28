e06277d370abbfd599812f360a5e3729b0399986
Tatiana Simmer <tatkagore@gmail.com>
2022-05-28 11:29:15 +0200

    Initial Commit

---
+++ Shared/Multiplication_TableApp.swift
@@ -0,0 +1,17 @@
+//
+//  Multiplication_TableApp.swift
+//  Shared
+//
+//  Created by Tatiana Simmer on 28/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct Multiplication_TableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
